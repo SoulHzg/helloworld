76ef87ce51a61c6049c05a6bb4f9cb2bce0e1492
soul <845948188@qq.com>
2020-08-04 08:36:54 +0800

    运算符+print高级用法

---
+++ Include/datatype/print高级用法.py
@@ -0,0 +1,5 @@
+print("a",end = "")  #end默认换行是\n
+print("a",end = "")
+print("a")
+
+print(1,2,3,sep=",")
--- Include/datatype/运算符.py
+++ Include/datatype/运算符.py
@@ -1,5 +1,9 @@
+import math
+
 num1=100
 num2=15
 print(num1//num2) #运算符取整
 print(num1%num2) #运算符取余
 print(num1**2) #运算符开方
+print(math.pow(4,2))  #平方
+print(math.sqrt(4)) #平方根
